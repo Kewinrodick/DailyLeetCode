c06295381412812e08c8c40cc2af9a00d36e0a5b
Kewin Rodick M <rodickkewin@gmail.com>
2026-02-10 06:59:59 +0530

    Added solution - LeetHub

--- 707364-split-array-largest-sum-GFG/707364-split-array-largest-sum.java
+++ 707364-split-array-largest-sum-GFG/707364-split-array-largest-sum.java
@@ -1,36 +1,40 @@
 class Solution {
     public int splitArray(int[] arr, int k) {
         // code here
-     int n = arr.length;
-        int l = Integer.MIN_VALUE;
-        int h = 0;
+        int n = arr.length;
         
-        for(int i:arr){
+        int l = Integer.MIN_VALUE ,h=0;
+        
+        for(int i: arr){
             l = Math.max(l,i);
-            h += i;
+            h +=i;
         }
         
-        while(l<=h){
+        while(l <= h){
             int m = l + (h-l)/2;
             if(isPossible(arr,k,m)){
-                h = m-1;
+                h=m-1;
             }
             else{
-                l = m+1;
+                l=m+1;
             }
+            
         }
         return h+1;
     }
     
     boolean isPossible(int[] arr,int k,int m){
-        int sum = 0,c = 1;
+        int c = 1, lsum = 0;
+        
         for(int i:arr){
-            if(sum+i <= m){
-                sum+=i;
+            if(lsum + i <= m){
+                lsum+=i;
             }
             else{
-                c++;
-                sum = i;
+               
+                    c++;
+                    lsum = i;
+                
             }
             
             if(c > k){
