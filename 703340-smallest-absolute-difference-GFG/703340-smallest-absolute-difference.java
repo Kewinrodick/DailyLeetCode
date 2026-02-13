5c1cb8c6472c35215b721ffa8c77bd1f9b1aefee
Kewin Rodick M <rodickkewin@gmail.com>
2026-02-13 07:33:17 +0530

    Added solution - LeetHub

---
+++ 703340-smallest-absolute-difference-GFG/703340-smallest-absolute-difference.java
@@ -0,0 +1,39 @@
+// User function Template for Java
+
+class Compute {
+
+    public long kthDiff(int nums[], int n, int k) {
+
+        Arrays.sort(nums);
+
+        int h = nums[n - 1] - nums[0];
+        int l = Integer.MAX_VALUE;
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] - nums[i - 1] < l)
+                l = nums[i] - nums[i - 1];
+        }
+
+        while (l <= h) {
+            int m = l + (h - l) / 2;
+            if (countPairs(nums, m) < k) {
+                l = m + 1;
+            } else {
+                h = m - 1;
+            }
+
+        }
+        return l;
+    }
+
+    int countPairs(int[] arr, int diff) {
+        int j = 0;
+        int count = 0, n = arr.length;
+        for (int i = 1; i < n; i++) {
+            while (arr[i] - arr[j] > diff)
+                j++;
+            count += i - j;
+        }
+        return count;
+    }
+}
