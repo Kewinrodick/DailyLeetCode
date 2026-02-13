cd916de324dc22ca407fdcc0a4ec929f4bbe2fdf
Kewin Rodick M <rodickkewin@gmail.com>
2026-02-13 07:48:59 +0530

    Added solution - LeetHub

--- 703958-largest-prime-factor-GFG/703958-largest-prime-factor.java
+++ 703958-largest-prime-factor-GFG/703958-largest-prime-factor.java
@@ -4,14 +4,14 @@ class Solution {
     static int largestPrimeFactor(int n) {
         int ans = Integer.MIN_VALUE;
         // code here
-        while(n!=1){
+        
             for(int i=2;i<=n;i++){
                while(n%i == 0 && n>0){
                    ans = Math.max(i,ans);
                    n/=i;
                }
             }
-        }
+        
         
         return ans;
     }
