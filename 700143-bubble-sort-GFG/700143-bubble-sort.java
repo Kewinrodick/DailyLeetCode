78491174d2abbda13ce74949663f2eb4fb03301e
Kewin Rodick M <rodickkewin@gmail.com>
2026-02-18 06:54:03 +0530

    Added solution - LeetHub

--- 700143-bubble-sort-GFG/700143-bubble-sort.java
+++ 700143-bubble-sort-GFG/700143-bubble-sort.java
@@ -6,9 +6,9 @@ class Solution {
         if(n<2)return;
         
         for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
-                if(arr[j] < arr[i]){
-                    swap(arr,i,j);
+            for(int j=0;j<n-i-1;j++){
+                if(arr[j] > arr[j+1]){
+                    swap(arr,j,j+1);
                 }   
             }
         }
