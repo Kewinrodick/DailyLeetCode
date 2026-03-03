95241e887668c27b2c1ab107bf6f86bd7badc43f
Kewin Rodick M <rodickkewin@gmail.com>
2026-03-03 07:38:24 +0530

    Added solution - LeetHub

---
+++ 701211-trapping-rain-water-GFG/701211-trapping-rain-water.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxWater(int arr[]) {
+        // code here
+        int n = arr.length;
+        int l = 0,r=n-1,c=0;
+        
+        int lmax = 0,rmax=0;
+        
+        while(l<=r){
+            
+            if(arr[l]<=arr[r]){
+                lmax = Math.max(lmax,arr[l]);
+                c+=lmax - arr[l];
+                l++;
+            }
+            else{
+                rmax = Math.max(rmax,arr[r]);
+                c+=rmax - arr[r];
+                r--;
+            }
+        }  
+        
+        return c;
+    }
+}
