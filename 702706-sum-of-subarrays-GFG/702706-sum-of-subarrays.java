b4202ee32f6262f7a589a8b7fbf7f68672724420
Kewin Rodick M <rodickkewin@gmail.com>
2026-03-13 07:34:31 +0530

    Added solution - LeetHub

---
+++ 702706-sum-of-subarrays-GFG/702706-sum-of-subarrays.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int subarraySum(int[] arr) {
+        // code here
+        
+        int n = arr.length;
+        
+        int sum = 0;
+        
+        for(int i=0;i<n;i++){
+            sum+=(i+1)*(n-i)*arr[i];
+        }
+        return sum;
+        
+    }
+}
