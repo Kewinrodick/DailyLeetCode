82124f6d8b8f492e92e2a048e439c36974a339a2
Kewin Rodick M <rodickkewin@gmail.com>
2026-03-15 19:33:34 +0530

    Added solution - LeetHub

--- 703205-longest-substring-with-distinct-characters-GFG/703205-longest-substring-with-distinct-characters.java
+++ 703205-longest-substring-with-distinct-characters-GFG/703205-longest-substring-with-distinct-characters.java
@@ -1,19 +1,22 @@
 class Solution {
     public int longestUniqueSubstr(String s) {
         // code here
-        HashMap<Character,Integer> map = new HashMap<>();
-        int ans = 0,i=0;
-        
-        for(int j=0;j<s.length();j++){
-            
-            while(map.containsKey(s.charAt(j))){
-                 map.remove(s.charAt(i));
-                 i++;
-            }
-            
-            map.put(s.charAt(j), j);
-            ans = Math.max(ans,(j-i)+1);
-        }
-        return ans;
+        HashSet<Character> set = new HashSet<>();
+	    	int ans = 0,i,j=0;
+	    	for(i=0;i<s.length();i++) {
+	    		char curr = s.charAt(i);
+	    		
+	    		while(set.contains(s.charAt(i))){
+	    			set.remove(s.charAt(j));
+	    			j++;
+	    		}
+	    		
+	    		set.add(s.charAt(i));
+	    		ans = Math.max(ans, i-j+1);
+	    	
+	    		
+	    	}
+	    	ans = Math.max(ans, i-j);
+	    	return ans;
     }
 }
