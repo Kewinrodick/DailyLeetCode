ca63ded02539fae73fdbf4ebdec84db76f9cc633
Kewin Rodick M <rodickkewin@gmail.com>
2026-03-23 11:49:45 +0530

    Time: 37 ms (20%), Space: 47.3 MB (7.7%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -4,25 +4,25 @@ class Solution {
         int m = s2.length();
         if(n>m) return false;
 
-        char[] checkArr = s1.toCharArray();
-        Arrays.sort(checkArr);
+        HashMap<Character,Integer> s1Map = new HashMap<>();
+        HashMap<Character,Integer> s2Map = new HashMap<>();
 
-        String sortS1 = new String(checkArr);
+        for(int i= 0;i<n;i++){
+            s1Map.put(s1.charAt(i),s1Map.getOrDefault(s1.charAt(i),0)+1);
+            s2Map.put(s2.charAt(i),s2Map.getOrDefault(s2.charAt(i),0)+1);
+        }
+        if(s1Map.equals(s2Map)) return true;
 
-        int i = 0;
-        for(int j=0;j<m;j++){
-            if(j-i+1 == n){
-                String currStr = s2.substring(i,j+1);
-                char[] checkS2 = currStr.toCharArray();
-                Arrays.sort(checkS2);
-                String sortS2 = new String(checkS2);
-                if(sortS1.equals(sortS2)){
-                    return true;
-                }
-                else{
-                    i++;
-                }
-            }
+        int i=0;   
+        for(int j=n;j<m;j++){
+           s2Map.put(s2.charAt(j),s2Map.getOrDefault(s2.charAt(j),0)+1);
+           s2Map.put(s2.charAt(i),s2Map.getOrDefault(s2.charAt(i),0)-1);
+           if(s2Map.get(s2.charAt(i))==0){
+            s2Map.remove(s2.charAt(i));
+           }
+           i++;
+
+           if(s1Map.equals(s2Map)) return true;
         }
         return false;
     }
