ee76530eecf5c8a26a7fd2fb6014d74eff4aafb7
Kewin Rodick M <rodickkewin@gmail.com>
2026-03-25 11:08:32 +0530

    Added solution - LeetHub

--- 700203-dfs-of-graph-GFG/700203-dfs-of-graph.java
+++ 700203-dfs-of-graph-GFG/700203-dfs-of-graph.java
@@ -1,28 +1,24 @@
 class Solution {
-    public void traverse(int node,Queue<Integer> queue,int[] visited,ArrayList<ArrayList<Integer>> adj,ArrayList<Integer> ans){
+    public void traverse(int node,int[] visited,ArrayList<ArrayList<Integer>> adj,ArrayList<Integer> ans){
          visited[node] = 1;
-         queue.offer(node);
+         ans.add(node);
          
-         while(!queue.isEmpty()){
-            int curr = queue.poll();
-            ans.add(curr);
-            for(int i:adj.get(curr)){
+         
+            for(int i:adj.get(node)){
                 if(visited[i] != 1){
-                    traverse(i,queue,visited,adj,ans);
+                    traverse(i,visited,adj,ans);
                 }
             }
-        }
+        
     }
     public ArrayList<Integer> dfs(ArrayList<ArrayList<Integer>> adj) {
         // code here
-        
-        Queue<Integer> queue = new LinkedList<>();
         
         ArrayList<Integer> ans = new ArrayList<>();
         
         int[] visited = new int[adj.size()];
         
-        traverse(0,queue,visited,adj,ans);
+        traverse(0,visited,adj,ans);
         
         return ans;
        
