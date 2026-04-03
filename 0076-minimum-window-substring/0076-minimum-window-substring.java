abaa9bd6b7963814ec8d88c37a0d8ce3a0f45931
Kewin Rodick M <rodickkewin@gmail.com>
2026-04-03 21:38:29 +0530

    Time: 23 ms (23.34%), Space: 47.6 MB (36.09%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,45 +1,51 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int l1 = s.length();
-        int l2 = t.length();
 
-        if(l2 > l1) return "";
-    
-        if(l1 == 1 && l2 == 1 && !s.equals(t))return ""; 
+        if (s.length() < t.length()) return "";
 
-        HashMap<Character,Integer> map = new HashMap<>();
-
-        for(char i :t.toCharArray()){
-            map.put(i,map.getOrDefault(i,0)+1);
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char c : t.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
         }
 
+        int required = map.size();
+        int formed = 0;
+
+        HashMap<Character, Integer> window = new HashMap<>();
+
+        int l = 0, r = 0;
         int minLen = Integer.MAX_VALUE;
-        int start = -1;
-        int c = 0;
-        int i=0;
-        for(int j=0;j<l1;j++){
-            char curr = s.charAt(j);
-            if(map.containsKey(curr)){
-                if(map.get(curr) > 0){
-                    c++;
-                }
-            }
-            map.put(curr,map.getOrDefault(curr,0)-1);
-     
-            while(c == l2){
-                map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
-                if(map.get(s.charAt(i)) > 0){
-                    c--;
-                }
-                if(minLen > j-i+1){
-                    minLen = j-i+1;
-                    start = i;
-                }
-                i++;
-            }
-        }
-        if(start == -1)return "";
-        return s.substring(start,start+minLen);
+        int start = 0;
 
+        while (r < s.length()) {
+            char c = s.charAt(r);
+
+            window.put(c, window.getOrDefault(c, 0) + 1);
+
+            if (map.containsKey(c) && window.get(c).intValue() == map.get(c).intValue()) {
+                formed++;
+            }
+
+            while (formed == required) {
+
+                if (r - l + 1 < minLen) {
+                    minLen = r - l + 1;
+                    start = l;
+                }
+
+                char left = s.charAt(l);
+                window.put(left, window.get(left) - 1);
+
+                if (map.containsKey(left) && window.get(left) < map.get(left)) {
+                    formed--;
+                }
+
+                l++;
+            }
+
+            r++;
+        }
+
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(start, start + minLen);
     }
 }
